b9f1337669fed26fa14fe81f4034e36b06b90864
hwi215 <buc06137@naver.com>
2025-05-21 14:38:05 +0900

    fix: add HealthData model

--- candiyHC/src/main/java/com/candiy/candiyhc/data/local/model/HealthData.kt
+++ candiyHC/src/main/java/com/candiy/candiyhc/data/local/model/HealthData.kt
@@ -1,5 +1,7 @@
 package com.candiy.candiyhc.data.local.model
 
+import com.squareup.moshi.Json
+
 data class StepData(
     val count: Int
 )
@@ -11,4 +13,24 @@ data class HeartRateSample(
 
 data class HeartRateData(
     val samples: List<HeartRateSample>
+)
+
+data class SleepData(
+    @Json(name = "sleep_duration_total")
+    val sleepDurationTotal: String,  // ISO-8601 duration format (e.g., "PT4H9M")
+
+    val stages: List<SleepStage>,
+
+    val notes: String,
+    val title: String
+)
+
+data class SleepStage(
+    val startTime: String,  // or Instant if you parse ISO strings
+    val endTime: String,
+    val stage: Int
+)
+
+data class OxygenSaturationData(
+    val percentage: Double
 )
